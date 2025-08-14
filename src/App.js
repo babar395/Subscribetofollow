ee27a5793eed5057b7f46692e07d678483977ed6
babar395 <babarbhutta395@gmail.com>
2025-08-14 13:32:42 +0500

    Add App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>SubscribeToFollow</h1>
+      <p>Welcome! This is your React App.</p>
+    </div>
+  );
+}
+
+export default App;
